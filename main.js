387b84e7ab1068c963acd6eb3157d4758128a87b
jbroom86 <jbroom86@gmail.com>
2023-02-19 15:54:56 -0800

    Added html, css, and js for photo carousel on About Me.

--- main.js
+++ main.js
@@ -25,3 +25,33 @@ menuItems.forEach(
       menuItem.addEventListener("click", toggleMenu);
     }
   )
+
+
+  let slideIndex = 1;
+showSlides(slideIndex);
+
+
+function plusSlides(pic1) {
+  showSlides(slideIndex += pic1);
+}
+
+
+function currentSlide(pic2) {
+  showSlides(slideIndex = pic2);
+}
+
+function showSlides(slide) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (slide > slides.length) {slideIndex = 1}
+  if (slide < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
