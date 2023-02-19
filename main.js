d6ad83e7ec4b5386dc4367c20d7d1ea609e9ca3c
jbroom86 <jbroom86@gmail.com>
2023-02-18 19:12:04 -0800

    Added comments to main.js.

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// Hamburger Menu Functionality //
+
 const menu = document.querySelector(".menu");
 const menuItems = document.querySelectorAll(".menuItem");
 const hamburger= document.querySelector(".hamburger");
